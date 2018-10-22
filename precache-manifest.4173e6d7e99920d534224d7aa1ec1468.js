48b6683171e3c55540c4457191353aa8afa37d0c
Saung Park <spark89@gmail.com>
2018-10-22 18:34:01 -0400

    Updates

---
+++ precache-manifest.4173e6d7e99920d534224d7aa1ec1468.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "08fc0e4a48db09b1ae22c2a0337025ae",
+    "url": "/static/media/wikiViewer.08fc0e4a.png"
+  },
+  {
+    "revision": "e86ceb89b97e60702a18",
+    "url": "/static/css/main.5343bbd0.chunk.css"
+  },
+  {
+    "revision": "f55e3f0e1df7b675d1db",
+    "url": "/static/js/1.f55e3f0e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "357e969750205ebf87d69bc44877e116",
+    "url": "/static/media/logo.357e9697.png"
+  },
+  {
+    "revision": "9aacc138c53773f1e87eb9015500d742",
+    "url": "/static/media/self-logo.9aacc138.jpg"
+  },
+  {
+    "revision": "dd0756fb7dd306995e525d22a021a744",
+    "url": "/static/media/memory-game.dd0756fb.PNG"
+  },
+  {
+    "revision": "e86ceb89b97e60702a18",
+    "url": "/static/js/main.e86ceb89.chunk.js"
+  },
+  {
+    "revision": "16bb9a6fefbd1640d6a4e43b22a9a5e1",
+    "url": "/static/media/react-maps.16bb9a6f.png"
+  },
+  {
+    "revision": "ded62f6d0e9f3b7b2933a9187cd8e3b9",
+    "url": "/static/media/myreads.ded62f6d.png"
+  },
+  {
+    "revision": "1aa2c012eb4dcf50a95dba9175c214ff",
+    "url": "/static/media/fitmuse.1aa2c012.png"
+  },
+  {
+    "revision": "355f6be59b5e1116c17bd73554a43020",
+    "url": "/static/media/sparky-pictionary.355f6be5.png"
+  },
+  {
+    "revision": "8c454bd6fe10fa78adeff809fbff06d2",
+    "url": "/static/media/chart-stock-history.8c454bd6.png"
+  },
+  {
+    "revision": "04062cc313c70e44b98a3fb2790a898b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.22cf102104aef83d9f2298bbacb251e5.js"
+  "/precache-manifest.4173e6d7e99920d534224d7aa1ec1468.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
